339d5c9a57086d47bc7570bad3e3433873d867fa
Abdulaziz20007 <92323615+Abdulaziz20007@users.noreply.github.com>
2023-04-19 10:58:09 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,25 @@
+// document.querySelector('#icon').oninput = function() {
+//     geticon()
+//   };
+// document.querySelector('#color').oninput = function() {
+//     geticon()
+//   };
+// function checkboxx() {
+// 	if(document.querySelector('#checkbox').checked == true) {
+// 		document.querySelector('#color').value.disabled = true;
+// 	}
+// 	else {
+// 		document.querySelector('#color').value.disabled = false;
+// 	}
+// 	geticon()
+// 	console.log(document.querySelector('#checkbox'));
+//   };
+//   function geticon() {
+// 	if(document.querySelector('#checkbox').checked == false) {
+// 		document.querySelector('iframe').src = 'https://img.icons8.com/'+document.querySelector('#color').value.substring(1)+'/'+document.querySelector('#icon').value
+// 	}
+// 	else {
+// 		document.querySelector('iframe').src = 'https://img.icons8.com/color/'+document.querySelector('#icon').value
+// 	}
+// 	console.log(document.querySelector('iframe').src);
+// }
